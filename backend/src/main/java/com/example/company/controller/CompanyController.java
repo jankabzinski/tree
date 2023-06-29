cdedbedf2355f3f3d9d138b463ad0322b86b669c
jankabzinski <jankabzinski05@gmail.com>
2023-06-29 11:29:20 +0200

    fixes in sorting, endpoint for req counter

--- backend/src/main/java/com/example/company/controller/CompanyController.java
+++ backend/src/main/java/com/example/company/controller/CompanyController.java
@@ -7,8 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.data.domain.Sort.Direction;
 
-import java.util.Comparator;
-import java.util.List;
 import java.util.Objects;
 
 import org.springframework.web.bind.annotation.RequestMethod;
--- backend/src/main/java/com/example/company/controller/RequestCounter.java
+++ backend/src/main/java/com/example/company/controller/RequestCounter.java
@@ -1,18 +1,28 @@
 package com.example.company.controller;
 
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Component
-@Getter
 @NoArgsConstructor
+@RestController
+@RequestMapping("/req")
+
 public class RequestCounter {
 
     private final AtomicInteger count = new AtomicInteger(0);
 
     public void incrementCount() {
         count.incrementAndGet();
+    }
+
+    @GetMapping()
+    public String getCount() {
+        return "Number of requests made: " + this.count.get();
     }
 }
