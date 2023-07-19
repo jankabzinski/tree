07f2f9f7dad723d10cedb3422540144f237cd42d
jankabzinski <jankabzinski05@gmail.com>
2023-07-19 18:49:03 +0200

    fixed vulnerabilities, tree design fixes

--- frontend/src/app/tree/tree.component.ts
+++ frontend/src/app/tree/tree.component.ts
@@ -1,21 +1,70 @@
 // tree.component.ts
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-tree',
-  templateUrl: './tree.component.html',
+  styleUrls: ['./tree.component.css'],
+  template: `
+    <ngx-graph [links]="links" [nodes]="nodes" [autoCenter]="true">
+      <ng-template #defsTemplate>
+        <svg:marker id="arrow" viewBox="0 -5 10 10" refX="8" refY="0" markerWidth="4" markerHeight="4" orient="auto">
+          <svg:path d="M0,-5L10,0L0,5" class="arrow-head"/>
+        </svg:marker>
+      </ng-template>
+
+      <ng-template #clusterTemplate let-cluster>
+        <svg:g class="node cluster">
+          <svg:rect
+            rx="5"
+            ry="5"
+            [attr.width]="cluster.dimension.width"
+            [attr.height]="cluster.dimension.height"
+            [attr.fill]="cluster.data.color"
+          />
+        </svg:g>
+      </ng-template>
+
+      <ng-template #nodeTemplate let-node>
+        <svg:g class="node">
+          <svg:rect
+            [attr.width]="node.dimension.width"
+            [attr.height]="node.dimension.height"
+            [attr.fill]="node.data.color"
+          />
+          <svg:text alignment-baseline="central" [attr.x]="10" [attr.y]="node.dimension.height / 2">
+            {{node.label}}
+          </svg:text>
+        </svg:g>
+      </ng-template>
+
+      <ng-template #linkTemplate let-link>
+        <svg:g class="edge">
+          <svg:path class="line" stroke-width="2" marker-end="url(#arrow)"></svg:path>
+          <svg:text class="edge-label" text-anchor="middle">
+            <textPath
+              class="text-path"
+              [attr.href]="'#' + link.id"
+              [style.dominant-baseline]="link.dominantBaseline"
+              startOffset="50%"
+            >
+              {{link.label}}
+            </textPath>
+          </svg:text>
+        </svg:g>
+      </ng-template>
+    </ngx-graph>`
 })
 export class TreeComponent {
   nodes = [
-    { id: '1', label: 'Node 1' },
-    { id: '2', label: 'Node 2' },
-    { id: '3', label: 'Node 3' },
+    {id: '1', label: 'Node 1'},
+    {id: '2', label: 'Node 2'},
+    {id: '3', label: 'Node 3'},
     // Dodaj pozostałe węzły...
   ];
 
   links = [
-    { source: '1', target: '2' },
-    { source: '2', target: '3' },
+    {source: '1', target: '2'},
+    {source: '3', target: '1', id: '2'}
     // Dodaj pozostałe krawędzie...
   ];
 }
