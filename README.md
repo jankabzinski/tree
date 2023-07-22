2b42534fbda535a0c85847cb7cc1b960f0bcc176
jankabzinski <jankabzinski05@gmail.com>
2023-07-23 00:36:09 +0200

    Dodanie skrypt.sql i napisanie README.md

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 ## Spring Boot + Angular app with a PostgreSQL database
+1. Baza danych: PostgreSQL, PgAdmin 4
+Utwórz bazę PostgreSQL w PgAdmin i wykonaj w niej skrypt.sql
 
-Utwórz bazę PostgreSQL i wykonaj w niej skrypt.sql
+2. Backend i połączenie z bazą danych: InteliIJ, Java 17 
+Otwórz folder backend w wybranym przez siebie IDE. Zdefiniuj jako zmienne środowiskowe: 
+ - url - pod którym będziemy się łączyć z bazą. Jeśli zrobiłeś domyślną konfigurację w PostgreSQl, deklaracja zmiennej będzie wyglądać, dla bazy nazwanej "tree" tak:
+   url=jdbc:postgresql://localhost:5432/tree
+- nazwa użytkownika postgres oraz hasło - definiowałeś je przy pierwszym włączeniu PgAdmin. Deklaracja zmiennych dla nazwy użykownika i hasła:
+username=twoja_nazwa_uzytkownika;password=twoje_haslo
 
-Na backendzie ustaw zmienne środowiskowe url (adres bazy danych), username i password (nazwa użykownika i hasło do bazy).
-Backend działa na localhost:8080, a frontend localhost:4200
+Zmienne środowiskowe możesz również zahardkodować w application.properties (niezalecane).
+
+Następnie uruchom funkcję main w klasie NodeApplication. Aplikacja będzie działać pod adresem: localhost:8080/nodes
+
+
+3. Frontend: Angular CLI: 16.1.1, Node: 16.17.0
+W terminalu wejdź do folderu frontend i wpisz komendę ng serve. Aplikacja będzie działać pod adresem: localhost:4200/nodes
--- skrypt.sql
+++ skrypt.sql
@@ -1,10 +1,21 @@
-DROP TABLE node;
-CREATE TABLE node(
-        id INT NOT NULL  PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1)
-    ,name VARCHAR(11) NOT NULL
-    ,JOB VARCHAR(10) NOT NULL);
+drop table if exists node;
 
-INSERT INTO node values (100,'kowalski', 'boss');
-INSERT INTO node values (200,'mnich', 'manager');
-INSERT INTO node values (300,'nowak', 'intern');
-INSERT INTO node values (400,'vader', 'developer');
+create table node
+(
+    id        serial
+        primary key,
+    parent_id bigint
+                                references node
+                                    on delete set null,
+    value     integer default 0 not null,
+    sum       integer default 0 not null
+);
+
+
+INSERT INTO node (value, sum) VALUES (10, 10);
+INSERT INTO node (parent_id, value, sum) VALUES (1, 5, (SELECT COALESCE(SUM(value), 0) + 5 FROM node WHERE id = 1));
+INSERT INTO node (parent_id, value, sum) VALUES (2, 7, (SELECT COALESCE(SUM(value), 0) + 7 FROM node WHERE id = 2));
+INSERT INTO node (parent_id, value, sum) VALUES (3, 14, (SELECT COALESCE(SUM(value), 0) + 14 FROM node WHERE id = 3));
+INSERT INTO node (parent_id, value, sum) VALUES (2, 12, (SELECT COALESCE(SUM(value), 0) + 12 FROM node WHERE id = 2));
+INSERT INTO node (parent_id, value, sum) VALUES (4, 132, (SELECT COALESCE(SUM(value), 0) + 132 FROM node WHERE id = 4));
+INSERT INTO node (parent_id, value, sum) VALUES (1, 0, (SELECT COALESCE(SUM(value), 0) FROM node WHERE id = 2));
